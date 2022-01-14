b3886f1ef98468303d9c2708aee57418339c7d67
CARLOS VIERA <pisis_13_3@hotmail.com>
2022-01-13 23:17:52 -0500

    initialcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./components/grilla.css";
+
 
 function App() {
+  const [color, setColor] = useState("#000");
+  const [grid, setGrid] = useState(GenerarGrilla(10, 10));
+
+  const handleClick = (row, col) => {
+    setGrid((grid) =>
+      grid.map((r, i) =>
+        i === row ? r.map((c, j) => (j === col ? color : c)) : r
+      )
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <table className="grid">
+        <tbody>
+          {grid.map((row, i) => (
+            <tr key={i}>
+              {row.map((color, j) => (
+                <td
+                  style={{ backgroundColor: color }}
+                  onClick={() => handleClick(i, j)}
+                ></td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      
+      <div className="colors">
+      <strong>Set colores: </strong>
+      
+        <div
+          className={`color color-black ${color === "#000" ? "active" : null}`}
+          onClick={() => setColor("#000")}
+        ></div>
+        <div
+          className={`color color-blue ${
+            color === "#0074d9" ? "active" : null
+          }`}
+          onClick={() => setColor("#0074d9")}
+        ></div>
+        <div
+          className={`color color-green ${
+            color === "#2ecc40" ? "active" : null
+          }`}
+          onClick={() => setColor("#2ecc40")}
+        ></div>
+      </div>
+    </>
   );
 }
+export default App
 
-export default App;
+const GenerarGrilla = (n, m) => {
+  const grid = [];
+  for (let i = 0; i < n; i++) {
+    grid[i] = [];
+    for (let j = 0; j < m; j++) {
+      grid[i][j] = "#000";
+    }
+  }
+  return grid;
+};
