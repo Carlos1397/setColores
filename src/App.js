618ebfb4510eefc5987272c5864f515f7a05030b
CARLOS VIERA <pisis_13_3@hotmail.com>
2022-01-17 21:45:47 -0500

    Agregando Grilla.jsx

--- src/App.js
+++ src/App.js
@@ -1,68 +1,13 @@
-import { useState } from "react";
-import "./components/grilla.css";
+import React, { Component } from 'react';
+import Grilla from './components/grilla';
 
-
-function App() {
-  const [color, setColor] = useState("#000");
-  const [grid, setGrid] = useState(GenerarGrilla(10, 10));
-
-  const handleClick = (row, col) => {
-    setGrid((grid) =>
-      grid.map((r, i) =>
-        i === row ? r.map((c, j) => (j === col ? color : c)) : r
-      )
-    );
-  };
-
-  return (
-    <>
-      <table className="grid">
-        <tbody>
-          {grid.map((row, i) => (
-            <tr key={i}>
-              {row.map((color, j) => (
-                <td
-                  style={{ backgroundColor: color }}
-                  onClick={() => handleClick(i, j)}
-                ></td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      
-      <div className="colors">
-      <strong>Set colores: </strong>
-      
-        <div
-          className={`color color-black ${color === "#000" ? "active" : null}`}
-          onClick={() => setColor("#000")}
-        ></div>
-        <div
-          className={`color color-blue ${
-            color === "#0074d9" ? "active" : null
-          }`}
-          onClick={() => setColor("#0074d9")}
-        ></div>
-        <div
-          className={`color color-green ${
-            color === "#2ecc40" ? "active" : null
-          }`}
-          onClick={() => setColor("#2ecc40")}
-        ></div>
-      </div>
-    </>
-  );
-}
-export default App
-
-const GenerarGrilla = (n, m) => {
-  const grid = [];
-  for (let i = 0; i < n; i++) {
-    grid[i] = [];
-    for (let j = 0; j < m; j++) {
-      grid[i][j] = "#000";
-    }
+class App extends Component {
+  render(){
+    return(
+      <Grilla 
+      n={10}
+      m={10}/>
+    )
   }
-  return grid;
-};
+}
+export default App;
